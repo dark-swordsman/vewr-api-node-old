72ec9c99db0ee67f5c0be63a61562977f38bcdee
dark-swordsman <kylebarr54321@gmail.com>
2019-04-03 01:36:50 -0400

    added auth.js

---
+++ auth.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const UserInterface = require('./interfaces/user');
+
+const params = {
+  secret: 'fsE3K4l8InmD2o',
+  requestJWT: ExtractJwt.fromAuthHeaderAsBearerToken('jwt')
+};
+
+module.exports = function() {
+  const strat = new JwtStrategy(params, (payload, done) => {
+    UserInterface.findUserById(payload.id)
+    .then((user) => {
+      done(null, {
+        id: user._id
+      });
+    })
+    .catch((err) => {
+      done(new Error(err), null);
+    });
+  });
+
+  passport.use(strat);
+
+  return {
+    init: () => {
+      return passport.initialize();
+    },
+    authenticate: () => {
+      return passport.authenticate('jwt')
+    }
+  }
+}
